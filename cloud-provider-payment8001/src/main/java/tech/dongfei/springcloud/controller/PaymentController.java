2afa4e8c0402785b6023cc1e26017053d750576b
lidongfei <652117746@qq.com>
2021-01-14 00:26:56 +0800

    update 2021011401

--- cloud-provider-payment8001/src/main/java/tech/dongfei/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/tech/dongfei/springcloud/controller/PaymentController.java
@@ -1,10 +1,7 @@
 package tech.dongfei.springcloud.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tech.dongfei.springcloud.entities.CommonResult;
 import tech.dongfei.springcloud.entities.Payment;
 import tech.dongfei.springcloud.service.PaymentService;
@@ -19,7 +16,7 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @PostMapping(value = "/payment/create")
-    public CommonResult create(Payment payment){
+    public CommonResult create(@RequestBody Payment payment){
         int result = paymentService.create(payment);
         log.info("******插入结果"+result);
         if(result > 0){
