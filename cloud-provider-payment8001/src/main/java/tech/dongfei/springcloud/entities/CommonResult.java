7a090a5b5b5666d5c73a7cabb1f102dcbf6e84fd
lidongfei <652117746@qq.com>
2021-01-13 21:56:11 +0800

    update 2021011301

--- cloud-provider-payment8001/src/main/java/tech/dongfei/springcloud/entities/CommonResult.java
+++ cloud-provider-payment8001/src/main/java/tech/dongfei/springcloud/entities/CommonResult.java
@@ -12,9 +12,7 @@ public class CommonResult<T> {
     private String message;
     private T   data;
 
-    public CommonResult(Integer code, String message, T data) {
-        this.code = code;
-        this.message = message;
-        this.data = data;
+    public CommonResult(Integer code, String message) {
+        this(code, message, null);
     }
 }
