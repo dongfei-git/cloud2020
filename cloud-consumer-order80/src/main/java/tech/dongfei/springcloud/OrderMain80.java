cc1a02c27eb7adcdeefa2c0c7dbd6c828951bbf0
lidongfei <652117746@qq.com>
2021-01-14 23:36:18 +0800

    update 2021011406

--- cloud-consumer-order80/src/main/java/tech/dongfei/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/tech/dongfei/springcloud/OrderMain80.java
@@ -3,9 +3,12 @@ package tech.dongfei.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import tech.dongfei.myrule.MySelfRule;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MySelfRule.class)
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
